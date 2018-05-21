628f861154a30ad9676e93a776952b003ff95cd6
timothychan986 <32720097+timothychan986@users.noreply.github.com>
2018-05-21 20:20:55 +0100

    begin

---
+++ PriceCalculation/PriceCalculationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PriceCalculationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PriceCalculationTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f521e562-dd44-4d8c-aceb-02f6639e59ed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PriceCalculation/PriceCalculationTest/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PriceCalculationTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        private IPriceCalculator priceCalculator;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            //Arrange
+            priceCalculator = new PriceCalculator();
+        }
+
+        [Test]
+        public void GIVEN_the_basket_has_1_bread_1_butter_1_milk_WHEN_I_total_the_basket_THEN_the_total_should_be_2_95()
+        {
+            //Arrange
+
+
+            //Act
+
+            //Assert
+
+        }
+
+        [Test]
+        public void GIVEN_the_basket_has_2_butter_and_2_bread_WHEN_I_total_the_basket_THEN_the_total_should_be_3_10()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+
+        [Test]
+        public void GIVEN_the_basket_has_4_milk_WHEN_I_total_the_basket_THEN_total_should_be_3_45()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+
+        [Test]
+        public void GIVEN_the_basket_has_2_butter_1_bread_8_milk_WHEN_I_total_the_basket_THEN_the_total_should_be_9()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
