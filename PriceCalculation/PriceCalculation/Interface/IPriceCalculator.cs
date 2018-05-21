9a372a1a27182c11238c197cdc50f70b238bc641
timothychan986 <32720097+timothychan986@users.noreply.github.com>
2018-05-21 20:39:27 +0100

    add interface

--- PriceCalculation/PriceCalculation/Interface/IPriceCalculator.cs
+++ PriceCalculation/PriceCalculation/Interface/IPriceCalculator.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PriceCalculation
+namespace PriceCalculation.Interface
 {
-    public class Class1
+    public interface IPriceCalculator
     {
+
     }
 }
