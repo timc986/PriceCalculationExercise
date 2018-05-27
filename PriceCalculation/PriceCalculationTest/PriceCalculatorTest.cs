e93380d7b7d011bd3be7d9bd6fe60f0854b918bd
timothychan986 <32720097+timothychan986@users.noreply.github.com>
2018-05-27 17:06:29 +0100

    setup cost for basket in unit test

--- PriceCalculation/PriceCalculationTest/PriceCalculatorTest.cs
+++ PriceCalculation/PriceCalculationTest/PriceCalculatorTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using PriceCalculation.Interface;
 using PriceCalculation;
 using PriceCalculation.Model;
@@ -9,8 +8,8 @@ namespace PriceCalculationTest
     [TestFixture]
     public class PriceCalculatorTest
     {
+        //nothing to be mocked
         private IPriceCalculator priceCalculator;
-
 
         [SetUp]
         public void Setup()
@@ -23,12 +22,10 @@ namespace PriceCalculationTest
         public void GIVEN_the_basket_has_1_bread_1_butter_1_milk_WHEN_I_total_the_basket_THEN_the_total_should_be_2_95()
         {
             //Arrange
-            var request = new Basket()
-            {
-                Bread = new Bread() { Amount = 1},
-                Butter = new Butter() { Amount = 1},
-                Milk = new Milk() { Amount = 1}
-            };
+            var request = SetUpBasket();
+            request.Bread.Amount = 1;
+            request.Butter.Amount = 1;
+            request.Milk.Amount = 1;
 
             //Act
             var result = priceCalculator.Calculate(request);
@@ -41,11 +38,9 @@ namespace PriceCalculationTest
         public void GIVEN_the_basket_has_2_butter_and_2_bread_WHEN_I_total_the_basket_THEN_the_total_should_be_3_10()
         {
             //Arrange
-            var request = new Basket()
-            {
-                Bread = new Bread() { Amount = 2 },
-                Butter = new Butter() { Amount = 2 }
-            };
+            var request = SetUpBasket();
+            request.Bread.Amount = 2;
+            request.Butter.Amount = 2;
 
             //Act
             var result = priceCalculator.Calculate(request);
@@ -58,10 +53,8 @@ namespace PriceCalculationTest
         public void GIVEN_the_basket_has_4_milk_WHEN_I_total_the_basket_THEN_total_should_be_3_45()
         {
             //Arrange
-            var request = new Basket()
-            {
-                Milk = new Milk() { Amount = 4 }
-            };
+            var request = SetUpBasket();
+            request.Milk.Amount = 4;
 
             //Act
             var result = priceCalculator.Calculate(request);
@@ -74,12 +67,10 @@ namespace PriceCalculationTest
         public void GIVEN_the_basket_has_2_butter_1_bread_8_milk_WHEN_I_total_the_basket_THEN_the_total_should_be_9()
         {
             //Arrange
-            var request = new Basket()
-            {
-                Bread = new Bread() { Amount = 1 },
-                Butter = new Butter() { Amount = 2 },
-                Milk = new Milk() { Amount = 8 }
-            };
+            var request = SetUpBasket();
+            request.Bread.Amount = 1;
+            request.Butter.Amount = 2;
+            request.Milk.Amount = 8;
 
             //Act
             var result = priceCalculator.Calculate(request);
@@ -92,10 +83,8 @@ namespace PriceCalculationTest
         public void GIVEN_the_basket_has_1_bread_WHEN_I_total_the_basket_THEN_the_total_should_be_1()
         {
             //Arrange
-            var request = new Basket()
-            {
-                Bread = new Bread() { Amount = 1 }
-            };
+            var request = SetUpBasket();
+            request.Bread.Amount = 1;
 
             //Act
             var result = priceCalculator.Calculate(request);
@@ -108,10 +97,8 @@ namespace PriceCalculationTest
         public void GIVEN_the_basket_has_1_butter_WHEN_I_total_the_basket_THEN_the_total_should_be_0_8()
         {
             //Arrange
-            var request = new Basket()
-            {
-                Butter = new Butter() { Amount = 1 }
-            };
+            var request = SetUpBasket();
+            request.Butter.Amount = 1;
 
             //Act
             var result = priceCalculator.Calculate(request);
@@ -124,10 +111,8 @@ namespace PriceCalculationTest
         public void GIVEN_the_basket_has_1_milk_WHEN_I_total_the_basket_THEN_the_total_should_be_1_15()
         {
             //Arrange
-            var request = new Basket()
-            {
-                Milk = new Milk() { Amount = 1 }
-            };
+            var request = SetUpBasket();
+            request.Milk.Amount = 1;
 
             //Act
             var result = priceCalculator.Calculate(request);
@@ -140,12 +125,10 @@ namespace PriceCalculationTest
         public void GIVEN_the_basket_has_0_butter_0_bread_0_milk_WHEN_I_total_the_basket_THEN_the_total_should_be_0()
         {
             //Arrange
-            var request = new Basket()
-            {
-                Bread = new Bread() { Amount = 0 },
-                Butter = new Butter() { Amount = 0 },
-                Milk = new Milk() { Amount = 0 }
-            };
+            var request = SetUpBasket();
+            request.Bread.Amount = 0;
+            request.Butter.Amount = 0;
+            request.Milk.Amount = 0;
 
             //Act
             var result = priceCalculator.Calculate(request);
@@ -175,6 +158,16 @@ namespace PriceCalculationTest
 
             //Assert
             Assert.AreEqual(0, result);
+        }
+
+        private Basket SetUpBasket()
+        {
+            return new Basket()
+            {
+                Bread = new Bread(1),                
+                Butter = new Butter(0.8),
+                Milk = new Milk(1.15),
+            };
         }
     }
 }
