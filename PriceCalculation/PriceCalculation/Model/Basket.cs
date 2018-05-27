24e60f5084c63ff4ba81edd738d422158af24ace
timothychan986 <32720097+timothychan986@users.noreply.github.com>
2018-05-27 15:20:05 +0100

    create models

---
+++ PriceCalculation/PriceCalculation/Model/Basket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceCalculation.Model
+{
+    public class Basket
+    {
+        public Butter Butter { get; set; }
+
+        public Milk Milk { get; set; }
+
+        public Bread Bread { get; set; }
+    }
+}
---
+++ PriceCalculation/PriceCalculation/Model/Butter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceCalculation.Model
+{
+    public class Butter: Product
+    {
+        public readonly double Cost = 0.8;
+    }
+}
